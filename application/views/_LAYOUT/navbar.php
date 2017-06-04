61d61c3c052ccabd779a91b94c7a62e84bb11f00
chulbuji-kim <chulbuji.kim@gmail.com>
2017-06-04 16:41:32 +0900

    commit ref --first

---
+++ application/views/_LAYOUT/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default navbar-fixed-top gq-navbar">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/GOODS/">
+                GAPU
+            </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar gq-navbar-menu">
+                <li><a href="/GOODS/about">회사소개</a></li>
+                <li><a href="/GOODS/blog">게임목록</a></li>
+                <li><a href="/GOODS/gallery">공지사항</a></li>
+                <li><a href="/GOODS/gallery">고객지원</a></li>
+                <li><a href="/GOODS/gallery">채용정보</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
